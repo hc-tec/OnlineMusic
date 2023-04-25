4f792ed0aebbebb92b4c2d7ed2115c30355546f0
BINJUR <1533630870@qq.com>
2023-04-25 20:08:10 +0800

    一点点

--- OnlineMusicBackend/src/controller/user.controller.js
+++ OnlineMusicBackend/src/controller/user.controller.js
@@ -1,11 +1,56 @@
-const { createUser } = require('../service/user.service')
+const { createUser, hasUserByName } = require('../service/user.service')
 
 class UserController {
     async register(ctx) {
         const { user_name, password } = ctx.request.body
+
+        // 合法性判断
+        if(!user_name || !password) {
+            ctx.status = 400
+            ctx.body = {
+                code: '10001',
+                message: '用户名或密码不能为空',
+                result: ''
+            }
+            return
+        }
+        else if(user_name.length > 7 || password.length > 12) {
+            ctx.status = 400
+            ctx.body = {
+                code: '10002',
+                message: '用户名或密码长度过大',
+                result: ''
+            }
+            return
+        }
+        else if(/[^0-9A-z]/g.test(password)) {
+            ctx.status = 400
+            ctx.body = {
+                code: '10003',
+                message: '密码含非法字符',
+                result: ''
+            }
+            return
+        }
+        else if(await hasUserByName(user_name)) {
+            // 合理性判断，即查找有无重复用户
+            ctx.status = 400
+            ctx.body = {
+                code: '10004',
+                message: '用户已存在',
+                result: ''
+            }
+            return
+        }
+
+
         const res = await createUser(user_name, password)
-        ctx.body = res
-        
+        ctx.body = {
+            code: '0',
+            message: '注册用户成功',
+            result: res
+        }
+  
     }
 }
 
--- OnlineMusicBackend/src/service/user.service.js
+++ OnlineMusicBackend/src/service/user.service.js
@@ -1,10 +1,31 @@
 const User = require('../model/user.model')
 
 class UserService {
+
     async createUser(user_name, password) {
         // 插入数据，相当于users表执行了insert语句，以下简写了user_name: user_name...
         const res = await User.create({ user_name, password })
         return res.dataValues
+    }
+
+    async getUserIdByName(user_name) {
+        const res = await User.findOne({
+            attributes: ['id'],
+            where: {
+                user_name: user_name
+            }
+        })
+        return res.dataValues.id
+    }
+
+    async hasUserByName(user_name) {
+        const res = await User.findAll({
+            attributes: ['id'],
+            where: {
+                user_name: user_name
+            }
+        })
+        return res.length > 0 ? true : false
     }
 }
  
