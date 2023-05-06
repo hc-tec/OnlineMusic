499e0a8f0ae5187b7ef8f5db11c9a6282aa5e514
BINJUR <1533630870@qq.com>
2023-05-07 00:46:47 +0800

    哎

--- OnlineMusicFrontend/src/components/HelloWorld.vue
+++ OnlineMusicFrontend/src/components/HelloWorld.vue
@@ -1,40 +1,21 @@
 <script setup>
-import { ref } from 'vue'
 
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+ <div class="ma">
+  ma
+  <div class="de">
+    de
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style scoped lang="less">
+.ma{
+  color:@main-color;
+  .de{
+    color: royalblue;
+  }
 }
 </style>
--- OnlineMusicFrontend/vite.config.js
+++ OnlineMusicFrontend/vite.config.js
@@ -1,7 +1,20 @@
+import path from 'path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+        less: {
+            modifyVars: {
+                hack: `true; @import (reference) "${path.resolve("src/assets/index.less")}";`,
+            },
+            javascriptEnabled: true,
+        },
+    },
+  }
 })
+
